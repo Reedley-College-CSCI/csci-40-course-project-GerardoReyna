0adf8b0947745248da08ce01c49eb94c4f2b77e9
Gerardo Reyna-Gomez <0823566@MY.SCCCD.EDU>
2026-04-23 19:42:49 -0700

    Edited my code and added names, phone numbers, and relationships to this person.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,52 @@
 */
 
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
+
+struct Contacts {
+    string name;
+    string phone;
+    string category;
+};
+
+
 int main() {
-    cout << "this is a test" << endl;
+    // Contact list
+    vector <Contacts> directory = {
+        {"Michael Ruiz", "559-666-8887", "Friend"},
+        {"Jason Johnson", "559-522-6556", "Older Brother"},
+        {"Leslie Johnson", "467-333-9122", "Sister"},
+        {"Emily Williams", "559-010-5678", "College Friend"},
+        {"David Johnson", "559-010-9012", "Brother"},
+        {"Grace Johnson", "559-100-3456", "Wife"},
+        {"Jessica Taylor", "559-070-7890", "Old Supervisor"},
+        {"Chris Evans", "559-111-8899", "Childhood Friend"},
+        {"Amanda White", "559-100-3456", "Next Door Neighbor"},
+        {"Linda Martinez", "559-100-3456", "Co-worker"},
+        {"James Wilson", "559-100-3456", "Cousin"},
+        {"Maria Garcia", "559-100-3456", "Close Friend"},
+        {"Dr. Alan Gonzalez", "559-100-3456", "Current Doctor"},
+        {"Anthony Ruiz", "559-100-3456", "Landlord"},
+        {"Jayden Lee", "559-100-3456", "Mechanic"},
+        {"Sarah Ortiz", "559-100-3456", "Friend"},
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
